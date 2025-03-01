800e832ef666ddc2a9005a170c6cd11810fe8c35
Federico Borgo <federico.borgo@mail.polimi.it>
2025-03-01 14:16:03 +0100

    Creata classe ShipBoard per contenere le tiles che comporranno la nave.

--- src/main/java/it/polimi/softeng/is25am10/model/ShipBoard.java
+++ src/main/java/it/polimi/softeng/is25am10/model/ShipBoard.java
@@ -1,4 +1,113 @@
 package it.polimi.softeng.is25am10.model;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShipBoard {
+
+    private static final Tile WALL_TILE = new Tile(TilesType.WALL, "ssss");
+    private static final Tile EMPY_TILE = new Tile(TilesType.EMPTY, "ssss");
+
+    private static final Pair<Integer, Integer>[] WALL_POSITION = new Pair[]{
+            new Pair<>(0, 0),
+            new Pair<>(1, 0),
+            new Pair<>(3, 0),
+            new Pair<>(5, 0),
+            new Pair<>(6, 0),
+            new Pair<>(0, 1),
+            new Pair<>(6, 1),
+            new Pair<>(3, 4),
+
+    };
+
+    private final ShipContainer<Tile> board;
+    private final ShipContainer<Character> orientation;
+    private final List<Tile> booked;
+    private final List<Tile> trashed;
+
+    public ShipBoard(){
+        board = new ShipContainer<>(EMPY_TILE);
+        orientation = new ShipContainer<>('n');
+        trashed = new ArrayList<>();
+        booked = new ArrayList<>();
+
+        for(Pair<Integer, Integer> position : WALL_POSITION)
+            board.set(position.getKey(), position.getValue(), WALL_TILE);
+
+        board.set(3, 2, new Tile(TilesType.C_HOUSE, "uuuu"));
+    }
+
+    private boolean checkNear(int x, int y){
+        List<Result<Tile>> around = new ArrayList<>();
+
+        around.add(board.get(x+1, y));
+        around.add(board.get(x-1, y));
+        around.add(board.get(x, y+1));
+        around.add(board.get(x, y-1));
+
+        for(Result<Tile> result : around){
+            if(result.isAccepted() && result.getData() != WALL_TILE && result.getData() != EMPY_TILE)
+                return true;
+        }
+
+        return false;
+    }
+
+    public Result<Tile> setTile(int x, int y, Tile t, char ori){
+        Result<Tile> result = board.get(x, y);
+
+        if(!result.isAccepted())
+            return result;
+
+        Tile prev = result.getData();
+
+        if(prev == WALL_TILE)
+            return new Result<>(false, null, "cant place out of bound");
+
+        if(prev != EMPY_TILE)
+            return new Result<>(false, null, "occupied tile");
+
+        if(!checkNear(x, y))
+            return new Result<>(false, null, "cant be placed in the void");
+
+        orientation.set(x, y, ori);
+        return board.set(x, y, t);
+    }
+
+    public Result<Tile> getTile(int x, int y) {
+        return board.get(x, y);
+    }
+
+    public Result<Character> getOrientation(int x, int y) {
+        return orientation.get(x, y);
+    }
+
+    public Result<Tile> bookTile(Tile t){
+        if(booked.size() >= 2)
+            return new Result<>(false, null, "booked tiles full");
+
+        if(booked.contains(t))
+            return new Result<>(false, null, "already booked");
+
+        booked.add(t);
+        return new Result<>(true, t, null);
+    }
+
+    public Result<Tile> useBookedTile(Tile t, char ori, int x, int y){
+        if(!booked.contains(t))
+            return new Result<>(false, null, "not booked");
+
+        Result<Tile> result = setTile(x, y, t, ori);
+
+        if(result.isAccepted())
+            booked.remove(t);
+
+        return result;
+    }
+
+    public List<Tile> getBooked() {
+        return booked;
+    }
 }
---
+++ src/test/java/it/polimi/softeng/is25am10/model/ShipBoardTest.java
@@ -0,0 +1,96 @@
+package it.polimi.softeng.is25am10.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipBoardTest {
+    private ShipBoard shipBoard;
+
+    @BeforeEach
+    void initialize() {
+        shipBoard = new ShipBoard();
+    }
+
+    @Test
+    void testSetTile() {
+        Tile tile = new Tile(TilesType.ROCKET, "otus");
+        Result<Tile> result;
+
+        result = shipBoard.setTile(3, 2, tile, 'n');
+        assertFalse(result.isAccepted());
+        assertEquals("occupied tile", result.getReason());
+
+        result = shipBoard.setTile(0, 0, tile, 'n');
+        assertFalse(result.isAccepted());
+        assertEquals("cant place out of bound", result.getReason());
+
+        result = shipBoard.setTile(2, 2, tile, 'n');
+        assertTrue(result.isAccepted());
+        assertEquals(TilesType.ROCKET, shipBoard.getTile(2, 2).getData().getType());
+
+        result = shipBoard.setTile(0, 2, tile, 'n');
+        assertFalse(result.isAccepted());
+        assertEquals("cant be placed in the void", result.getReason());
+    }
+
+    @Test
+    void testGetTile() {
+        Result<Tile> result;
+
+        result = shipBoard.getTile(3, 2);
+        assertTrue(result.isAccepted());
+        assertEquals(TilesType.C_HOUSE, result.getData().getType());
+
+        result = shipBoard.getTile(0, 0);
+        assertTrue(result.isAccepted());
+        assertEquals(TilesType.WALL, result.getData().getType());
+
+        result = shipBoard.getTile(0, 1);
+        assertTrue(result.isAccepted());
+        assertEquals(TilesType.WALL, result.getData().getType());
+    }
+
+    @Test
+    void testBook(){
+        Tile tile1 = new Tile(TilesType.ROCKET, "otus");
+        Tile tile2 = new Tile(TilesType.DRILLS, "otus");
+        Tile tile3 = new Tile(TilesType.HOUSE, "otus");
+
+        Result<Tile> result;
+
+        result = shipBoard.bookTile(tile1);
+        assertTrue(result.isAccepted());
+        assertTrue(shipBoard.getBooked().contains(tile1));
+
+        result = shipBoard.bookTile(tile1);
+        assertFalse(result.isAccepted());
+        assertEquals("already booked", result.getReason());
+
+        result = shipBoard.bookTile(tile2);
+        assertTrue(result.isAccepted());
+        assertTrue(shipBoard.getBooked().contains(tile2));
+
+        result = shipBoard.bookTile(tile3);
+        assertFalse(result.isAccepted());
+        assertEquals("booked tiles full", result.getReason());
+    }
+
+    @Test
+    void testUseBookedTile(){
+        Tile tile = new Tile(TilesType.ROCKET, "otus");
+        Result<Tile> result;
+
+        shipBoard.bookTile(tile);
+        result = shipBoard.useBookedTile(tile, 'n',2, 2);
+
+        assertTrue(result.isAccepted());
+        assertFalse(shipBoard.getBooked().contains(tile));
+
+        result = shipBoard.getTile(2, 2);
+        assertTrue(result.isAccepted());
+        assertEquals(TilesType.ROCKET, result.getData().getType());
+    }
+
+}
