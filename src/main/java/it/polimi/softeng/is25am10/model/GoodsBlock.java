34fb778a5a035f935d716729f8c713c964c1fa12
Federico Borgo <federico.borgo@mail.polimi.it>
2025-03-01 08:35:05 +0100

    Sistemato GoodBlock e rimosso enable-preview di Java.

--- src/main/java/it/polimi/softeng/is25am10/model/GoodsBlock.java
+++ src/main/java/it/polimi/softeng/is25am10/model/GoodsBlock.java
@@ -2,22 +2,15 @@ package it.polimi.softeng.is25am10.model;
 
 public enum GoodsBlock {
     GREEN, YELLOW, BLUE, RED, EMPTY;
+
+    public int getValue(){
+        return switch (this) {
+            case GREEN -> 2;
+            case YELLOW -> 3;
+            case BLUE -> 1;
+            case RED -> 4;
+            case EMPTY -> 0;
+            default -> 0;
+        };
+    }
 }
-
-/*
-
-non mi era chiaro cosa faceva bene questa funzione: ho supposto che ritorna un valore di int
-unico per ogni tipo di enum
-
-
-public int getValue(){
-return switch (this) {
-        case GREEN -> 1;
-        case YELLOW -> 2;
-        case BLUE -> 3;
-        case RED -> 4;
-        case EMPTY -> 0;
-        }
-}
-
- */
