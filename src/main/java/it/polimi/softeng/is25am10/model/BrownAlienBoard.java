80ab53eb0e726a15200cf16b59a0ca4aaeef561e
Federico Borgo <federico.borgo@mail.polimi.it>
2025-03-03 15:29:05 +0100

    Creati placeholder per i contenitori delle risorse nella plancia nave.

---
+++ src/main/java/it/polimi/softeng/is25am10/model/BrownAlienBoard.java
@@ -0,0 +1,17 @@
+package it.polimi.softeng.is25am10.model;
+
+public class BrownAlienBoard extends ElementsBoard{
+    public BrownAlienBoard(ShipBoard board) {
+        super(board);
+    }
+
+    @Override
+    public Result<Integer> put(int x, int y, int qty) {
+        return null;
+    }
+
+    @Override
+    public Result<Integer> move(int fromx, int fromy, int tox, int toy, int qty) {
+        return null;
+    }
+}
---
+++ src/main/java/it/polimi/softeng/is25am10/model/GoodsBoard.java
@@ -0,0 +1,4 @@
+package it.polimi.softeng.is25am10.model;
+
+public class GoodsBoard {
+}
