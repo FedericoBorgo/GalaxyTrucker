4d7d7102aab2d7a7f568a23d2c6e346837a69f54
Tsvetelin Aleksiev <tsvetelinmetodiev.aleksiev@mail.polimi.it>
2025-03-04 15:33:32 +0100

    Implementazione BatteryBoard e aggiungi commenti/ordinato ElementsBoard.

--- src/main/java/it/polimi/softeng/is25am10/model/BatteryBoard.java
+++ src/main/java/it/polimi/softeng/is25am10/model/BatteryBoard.java
@@ -1,12 +1,27 @@
 package it.polimi.softeng.is25am10.model;
 
-public class BatteryBoard extends ElementsBoard{
+public class BatteryBoard extends ElementsBoard {
     public BatteryBoard(ShipBoard board) {
         super(board);
     }
 
     @Override
     public Result<Integer> put(int x, int y, int qty) {
+
+        // Get Tile from ShipBoard and do an out-of-bound check
+        Result<Tile> resBoard = board.getTile(x, y);
+        if(!resBoard.isAccepted())
+            return new Result<>(false,null,resBoard.getReason());
+        Tile tile = board.getTile(x, y).getData();
+
+        // Add batteries
+        if (tile.getType() == TilesType.BATTERY_2) {
+            set(x, y, 2 + get(x, y)); // get(x, y) should always be 0
+        } else if (tile.getType() == TilesType.BATTERY_3) {
+            set(x, y, 3 + get(x, y)); // get(x, y) should always be 0
+        } else {
+            return new Result<>(false, null, "tile is not a Battery Tile");
+        }
         return null;
     }
 }
