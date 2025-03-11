05776368d225c338f6101245345edaa55392f671
Federico Borgo <federico.borgo@mail.polimi.it>
2025-03-11 19:54:33 +0100

    Aggiunti commenti cards.

--- src/main/java/it/polimi/softeng/is25am10/model/cards/Card.java
+++ src/main/java/it/polimi/softeng/is25am10/model/cards/Card.java
@@ -31,18 +31,39 @@ public abstract class Card {
         this.board = board;
     }
 
+    /**
+     * Check if the player already set their input.
+     *
+     * @param player
+     * @return
+     */
     public boolean isRegistered(Player player) {
         return registered.containsValue(player);
     }
 
+    /**
+     * Check if the given player is the one that should declare the next
+     * input.
+     *
+     * @param player
+     * @return true if he's him, false if not
+     */
     protected boolean isCorrectOrder(Player player) {
         return registered.keySet().containsAll(board.getOrder().subList(0, board.getOrder().indexOf(player.getPawn())));
     }
 
+    /**
+     * Check if all the player gave their input
+     * @return
+     */
     protected boolean allRegistered(){
         return registered.size() == board.getOrder().size();
     }
 
+    /**
+     * Register a player as already given the input.
+     * @param player
+     */
     public void register(Player player){
         registered.put(player.getPawn(), player);
     }
