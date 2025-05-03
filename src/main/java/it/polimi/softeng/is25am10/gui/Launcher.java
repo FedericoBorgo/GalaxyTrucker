a04f576274cd25fad83fc9f2da927a630faa11a6
chiarablanda <chiara.blanda@mail.polimi.it>
2025-05-03 23:32:10 +0200

    gui building ship font e scroll panel background.

--- src/main/java/it/polimi/softeng/is25am10/gui/Launcher.java
+++ src/main/java/it/polimi/softeng/is25am10/gui/Launcher.java
@@ -2,7 +2,6 @@ package it.polimi.softeng.is25am10.gui;
 
 import it.polimi.softeng.is25am10.Controller;
 import it.polimi.softeng.is25am10.network.ClientInterface;
-import it.polimi.softeng.is25am10.network.rmi.RMIClient;
 import it.polimi.softeng.is25am10.network.socket.SocketClient;
 import it.polimi.softeng.is25am10.tui.PlaceholderCallback;
 import javafx.application.Application;
@@ -10,13 +9,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class Launcher extends Application {
 
     public static void main(String[] args) throws IOException {
         Controller.main(new String[]{"false"});
+
+        Font customFont = Font.loadFont(Launcher.class.getResourceAsStream("/gui/font.ttf"), 12);
 
         ClientInterface bot = new SocketClient("bot", "localhost", 1235, 1236);
 
@@ -38,6 +42,10 @@ public class Launcher extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
+        scene.getStylesheets().add(getClass().getResource("/gui/style.css").toExternalForm());
+
+
 
     }
 }
