223df89268e60b78398119497f603ab2cadebd6b
Mattia Braguti <mattia.braguti@mail.polimi.it>
2025-05-20 14:24:43 +0200

    aggiunto javadoc a Controller Asciiui Logger e PlaceholderCallback

--- src/main/java/it/polimi/softeng/is25am10/Logger.java
+++ src/main/java/it/polimi/softeng/is25am10/Logger.java
@@ -17,29 +17,58 @@ public class Logger {
     static final String WHITE = "\u001B[37m";
 
 
+    /**
+     * Generic print method. Prints a message on the console.
+     * @param prefix
+     * @param message
+     * @param color
+     */
     private static void print(String prefix, String message, String color) {
         if(SILENCE)
             return;
         System.out.println(RESET + color + "[" + prefix + "]" + message);
     }
 
+    /**
+     * Prints a server message to the console
+     * @param message
+     */
     public static void serverLog(String message) {
         print("SERVER", message, WHITE);
     }
 
+    /**
+     * Prints a message from the model to the console
+     * @param id
+     * @param message
+     */
     public static void modelLog(int id, String message) {
         print("MODEL", "[" + id + "]" + message, PURPLE);
     }
 
+    /**
+     * Prints a message from the socket connection to the console
+     * @param message
+     */
     public static void socketLog(String message) {
         print("SOCKET", message, YELLOW);
     }
 
+    /**
+     * Prints a message from the client to the console
+     * @param message
+     */
     public static void clientLog(String message) {
         print("CLIENT", message, BLUE);
     }
 
 
+    /**
+     * Log a message between the model and the player
+     * @param id
+     * @param player
+     * @param message
+     */
     public static void playerLog(int id, String player, String message){
         String prefix = isRMI() ? "RMI" : "SOCKET";
         print("MODEL",
@@ -48,6 +77,10 @@ public class Logger {
                         "[" + player + "]" + message, CYAN);
     }
 
+    /**
+     * Check if the client is running in RMI mode
+     * @return
+     */
     public static boolean isRMI(){
         try {
             RemoteServer.getClientHost();
