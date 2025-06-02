b865db05d658a4e710250a3b5de03d50da7ecf04
Tsvetelin Metodiev Aleksiev <Tsvetelinmetodiev.aleksiev@mail.polimi.it>
2025-06-02 22:44:25 +0200

    Aggiunto nuovo test per controller

--- src/test/java/it/polimi/softeng/is25am10/ControllerTest.java
+++ src/test/java/it/polimi/softeng/is25am10/ControllerTest.java
@@ -1,194 +1,23 @@
 package it.polimi.softeng.is25am10;
 
+import it.polimi.softeng.is25am10.network.ClientInterface;
+import it.polimi.softeng.is25am10.network.rmi.RMIClient;
+import it.polimi.softeng.is25am10.tui.PlaceholderCallback;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ControllerTest {
-
-    @Test
-    void loadEvent() {
-    }
+public class ControllerTest {
+    ClientInterface firstPlayer, secondPlayer;
+    String playerName = "testPlayer1";
 
     @Test
-    void loadController() {
-    }
-
-    @Test
-    void setCallback() {
-    }
-
-    @Test
-    void join() {
-    }
-
-    @Test
-    void pushState() {
-    }
-
-    @Test
-    void pushFinalCash() {
-    }
-
-    @Test
-    void pushCardData() {
-    }
-
-    @Test
-    void pushCardChanges() {
-    }
-
-    @Test
-    void waitFor() {
-    }
-
-    @Test
-    void gaveTile() {
-    }
-
-    @Test
-    void gotTile() {
-    }
-
-    @Test
-    void pushFlight() {
-    }
-
-    @Test
-    void pushPlayers() {
-    }
-
-    @Test
-    void askHowManyPlayers() {
-    }
-
-    @Test
-    void pushSecondsLeft() {
-    }
-
-    @Test
-    void pushDropped() {
-    }
-
-    @Test
-    void pushCannons() {
-    }
-
-    @Test
-    void ping() {
-    }
-
-    @Test
-    void moveTimer() {
-    }
-
-    @Test
-    void setReady() {
-    }
-
-    @Test
-    void quit() {
-    }
-
-    @Test
-    void setTile() {
-    }
-
-    @Test
-    void bookTile() {
-    }
-
-    @Test
-    void useBookedTile() {
-    }
-
-    @Test
-    void placeOpenTile() {
-    }
-
-    @Test
-    void remove() {
-    }
-
-    @Test
-    void getRemoved() {
-    }
-
-    @Test
-    void getShip() {
-    }
-
-    @Test
-    void init() {
-    }
-
-    @Test
-    void getReward() {
-    }
-
-    @Test
-    void placeReward() {
-    }
-
-    @Test
-    void dropReward() {
-    }
-
-    @Test
-    void getCash() {
-    }
-
-    @Test
-    void drop() {
-    }
-
-    @Test
-    void testDrop() {
-    }
-
-    @Test
-    void increaseCannon() {
-    }
-
-    @Test
-    void drawTile() {
-    }
-
-    @Test
-    void getSeenTiles() {
-    }
-
-    @Test
-    void giveTile() {
-    }
-
-    @Test
-    void getTileFromSeen() {
-    }
-
-    @Test
-    void drawCard() {
-    }
-
-    @Test
-    void setInput() {
-    }
-
-    @Test
-    void getCardData() {
-    }
-
-    @Test
-    void getVisible() {
-    }
-
-    @Test
-    void getEnginePower() {
-    }
-
-    @Test
-    void getCannonPower() {
+    public void mainTest() throws IOException, InterruptedException {
+        Controller.main(new String[]{"true"});
+        firstPlayer = new RMIClient(playerName, "localhost", 1234);
+        secondPlayer = new RMIClient("testPlayer2", "localhost", 1234);
+        assertEquals(playerName, firstPlayer.getPlayerName());
     }
 }
