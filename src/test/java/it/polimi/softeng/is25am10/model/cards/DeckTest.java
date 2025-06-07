66f8b1aff0280220bad0817fb9cc7ff5f238fa0e
Tsvetelin Metodiev Aleksiev <Tsvetelinmetodiev.aleksiev@mail.polimi.it>
2025-06-07 13:31:17 +0200

    test deck e tutte le classi cards

--- src/test/java/it/polimi/softeng/is25am10/model/cards/DeckTest.java
+++ src/test/java/it/polimi/softeng/is25am10/model/cards/DeckTest.java
@@ -2,9 +2,11 @@ package it.polimi.softeng.is25am10.model.cards;
 
 import it.polimi.softeng.is25am10.model.Model;
 import it.polimi.softeng.is25am10.model.Player;
+import it.polimi.softeng.is25am10.model.boards.Coordinate;
 import it.polimi.softeng.is25am10.model.boards.FlightBoard;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,35 +18,162 @@ public class DeckTest {
 
     @Test
     public void testDeck() {
-        // Initialize the model and flight board for testing
+        // Deck creation
         testModel = new Model(2, (model, type) -> {});
         testFlightBoard = new FlightBoard();
-
-        // Create a new Deck instance
         Deck testDeck = new Deck(testModel, testFlightBoard);
-
-
-        // Assertions
         assertNotNull(testDeck, "Deck should not be null after creation");
         assertNotNull(testDeck.getVisible(), "Deck's internal card list should not be null");
 
-        // test draw method
+        // Players creation
         Player player1 = new Player("Player1", FlightBoard.Pawn.YELLOW);
         Player player2 = new Player("Player2", FlightBoard.Pawn.BLUE);
         List<Player> players = new java.util.ArrayList<>();
         players.add(player1);
         players.add(player2);
+
+        // Debug with space
+        List<Card> testCards = new ArrayList<>();
+        testCards.addAll(Space.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+
         Card drawnCard = testDeck.draw(players);
         assertNotNull(drawnCard, "Drawn card should not be null");
+        testDeck.getRegistered();
 
-        // test various method --> can be done better with debug_cards
         testDeck.set(player1, CardInput.disconnected());
         boolean ready = testDeck.ready();
-        testDeck.getData();
-        testDeck.getRegistered();
+        CardData cardDataTest = testDeck.getData();
+        cardDataTest.toString();
         testDeck.play();
 
         Deck testDeck2 = new Deck(testModel, testFlightBoard);
         assertFalse(testDeck2.equals(testDeck), "Decks with different cards should not be equal");
+
+        // Debug stardust
+        testCards.clear();
+        testCards.addAll(Stardust.construct(testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug station
+        testCards.clear();
+        testCards.addAll(Station.construct(testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug planets
+        testCards.clear();
+        testCards.addAll(Planets.construct(testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+
+        testDeck.play();
+
+        // Debug meteors
+        testCards.clear();
+        testCards.addAll(Meteors.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug epidemic
+        testCards.clear();
+        testCards.addAll(Epidemic.construct(testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug abandonedShip
+        testCards.clear();
+        testCards.addAll(AbandonedShip.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug pirates
+        testCards.clear();
+        testCards.addAll(Pirates.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        drawnCard = testDeck.draw(players);
+        Pirates piratesCard = (Pirates) drawnCard;
+        piratesCard.rollDice();
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+
+        // Debug slavers
+        testCards.clear();
+        testCards.addAll(Slavers.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug smugglers
+        testCards.clear();
+        testCards.addAll(Smugglers.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        // Debug warzone
+        testCards.clear();
+        testCards.addAll(Warzone.construct(testModel, testFlightBoard));
+        testDeck.debug_setCards(testCards);
+        testDeck.draw(players);
+
+        testDeck.set(player1, CardInput.disconnected());
+        testDeck.ready();
+        cardDataTest = testDeck.getData();
+        cardDataTest.toString();
+        testDeck.play();
+
+        CardOutput cardOutput = new CardOutput();
+        cardOutput.addDestroyed("test", new Coordinate(1, 1));
     }
 }
---
+++ src/test/java/it/polimi/softeng/is25am10/model/playerTest.java
@@ -0,0 +1,43 @@
+package it.polimi.softeng.is25am10.model;
+
+import it.polimi.softeng.is25am10.model.boards.Coordinate;
+import it.polimi.softeng.is25am10.model.boards.FlightBoard;
+import it.polimi.softeng.is25am10.model.boards.GoodsBoard;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class playerTest {
+    private String name = "TestPlayer1";
+    Player player1 = new Player(name, FlightBoard.Pawn.RED);
+    private String name2 = "TestPlayer2";
+    Player player2 = new Player(name2, FlightBoard.Pawn.BLUE);
+
+    @Test
+    public void testPlayerCreation() {
+        assert player1.getName().equals(name);
+        assert player2.getName().equals(name2);
+        assert player1.getPawn() == FlightBoard.Pawn.RED;
+        assert player2.getPawn() == FlightBoard.Pawn.BLUE;
+    }
+
+    @Test
+    public void testPlayerCash() {
+        assert player1.getCash() == 0; // Default cash should be 0
+        assert player2.getCash() == 0; // Default cash should be 0
+    }
+
+    @Test
+    public void testPlayerBoard() {
+        assert player1.getBoard() != null; // Board should be initialized
+        assert player2.getBoard() != null; // Board should be initialized
+        assert player1.getGoodsReward() != null;
+        player1.setGoodsReward(List.of(new GoodsBoard.Type[]{GoodsBoard.Type.RED, GoodsBoard.Type.BLUE}));
+        assert player1.placeReward(GoodsBoard.Type.RED, new Coordinate(0, 0)) != null;
+        assert player1.placeReward(GoodsBoard.Type.GREEN, new Coordinate(0, 0)) != null;
+        player2.giveCash(100);
+        assert player2.getCash() == 100; // Cash should be updated correctly
+        assert player1.equals(player1);
+        assert !player1.equals(player2);
+    }
+}
